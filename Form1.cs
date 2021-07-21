237dac473367ce0d6be2927951ab766317ac4837
torincyie13 <torincyie13@gmail.com>
2021-07-21 15:35:22 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ex5_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection booksConnection;
+        SqlCommand authorsCommand;
+        SqlDataAdapter authorsAdapter;
+        DataTable authorsTable;
+        CurrencyManager authorsManager;
+        private void frmSQLTester_Load(object sender, EventArgs e)
+        {
+            string path = Path.GetFullPath("SQLBooksDB.mdf");
+            // connect to books database
+            booksConnection = new
+                SqlConnection("Data Source=.\\SQLEXPRESS01; AttachDBFilename=" + path + ";" +
+                "Integrated Security=True; Connect Timeout=30; User Instance=True");
+            booksConnection.Open();
+            // establish command object
+            authorsCommand = new SqlCommand("Select * from Authors ORDER BY Author", booksConnection);
+            //establish data adapter/data table
+            authorsAdapter = new SqlDataAdapter();
+            auhtorsAdapter.SelectCommand = authorsCommand;
+            authorsTable = new DataTable();
+            authorsAdapter.Fill(authorsTable);
+            // bind controls to data table
+            txtAuthorID.DataBindings.Add("Text", authorsTable, "Au_ID");
+            txtAuthorName.DataBindings.Add("Text", authorsTable, "Author");
+            txtYearBorn.DataBindings.Add("Text", authorsTable, "Year_Born");
+            // estalish currency managey
+            authorsManager = (CurrencyManager)
+                this.BindingContext[authorsTable];
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // close the connection
+            booksConnection.Close();
+            // dispose of the objects
+            booksConnection.Dispose();
+            authorsCommand.Dispose();
+            authorsAdapter.Dispose();
+            authorsTable.Dispose();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position++;
+        }
+    }
+}
+
